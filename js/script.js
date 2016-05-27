98db0641f09dc6cfedd18d536f2edce0850b5674
twilken <thimowilken@gmail.com>
2016-05-27 16:40:09 +0200

    Remove slideout, add basic filtering

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
-// Setup hamburger menu using the slideout library
-var slideout = new Slideout({
-	'panel': document.getElementById('map'),
-	'menu': document.getElementById('sidebar'),
-	'padding': 256,
-	'tolerance': 70
-});
+$(function() {
+    'use strict';
 
-// Register hamburger icon toggle event listener
-document.querySelector('#hamburger').addEventListener('click', function() {
-    slideout.toggle();
+    var ViewModel = function() {
+        var self = this;
+
+        self.places = ko.observableArray(
+            [
+                {
+                    location: "Hamburg",
+                    visible: ko.observable(true),
+                },
+                {
+                    location: "Hannover",
+                    visible: ko.observable(true),
+                },
+                {
+                    location: "Hammberbrook",
+                    visible: ko.observable(true),
+                },
+                {
+                    location: "Berlin",
+                    visible: ko.observable(true),
+                }
+            ]
+        );
+
+        self.filter = ko.observable("");
+
+        self.filter.subscribe(function(filterValue) {
+
+            filterValue = filterValue.toLowerCase();
+
+            ko.utils.arrayFilter(self.places(), function(place) {
+                var visibility = place.location.toLowerCase().startsWith(filterValue) ? true : false;
+                place.visible(visibility);
+            });
+        });
+    }
+
+    var vm = new ViewModel();
+    ko.applyBindings(vm);
 });
